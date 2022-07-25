5fa9ab6c7888929f5487bad08708216e23aa41f8
twkim5235 <sssunggure@naver.com>
2022-07-25 22:16:10 +0900

    [ADD] 주문 도메인 배송 정보 추가

--- src/main/java/com/example/ddd_start/domain/order/Order.java
+++ src/main/java/com/example/ddd_start/domain/order/Order.java
@@ -1,17 +1,22 @@
 package com.example.ddd_start.domain.order;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import lombok.Getter;
 
-@Entity(name = "order")
+@Entity(name = "orders")
 @Getter
 public class Order {
 
   @Id
   private Long id;
   private String orderNumber;
+  @Enumerated(value = EnumType.STRING)
   private OrderState orderState;
+  @Embedded
   private ShippingInfo shippingInfo;
 
 }
--- src/main/java/com/example/ddd_start/domain/order/ShippingInfo.java
+++ src/main/java/com/example/ddd_start/domain/order/ShippingInfo.java
@@ -2,12 +2,15 @@ package com.example.ddd_start.domain.order;
 
 import com.example.ddd_start.domain.common.Address;
 import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
 import lombok.Getter;
 
 @Getter
 @Embeddable
 public class ShippingInfo {
 
+  @Embedded
   private Address address;
+  @Embedded
   private Receiver receiver;
 }
