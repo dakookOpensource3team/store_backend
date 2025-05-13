0bc60b1ed19c02fa16e71d1f376b2a98f106690a
twkim5235 <sssunggure@naver.com>
2025-05-13 14:56:02 +0900

    [FIX] 리팩토링 - 최근조회 상품 limit 7 설정

--- src/main/java/com/example/ddd_start/product/application/service/LastlyRetrieveProductService.java
+++ src/main/java/com/example/ddd_start/product/application/service/LastlyRetrieveProductService.java
@@ -21,26 +21,29 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class LastlyRetrieveProductService {
-    private final MemberRepository memberRepository;
-    private final ProductRepository productRepository;
-    private final LastlyRetrieveProductRepository lastlyRetrieveProductRepository;
 
-    @Transactional
-    public void saveLastlyRetrieveProduct(Long memberId, Long productId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(EntityNotFoundException::new);
-        Product product = productRepository.findById(productId).orElseThrow(EntityNotFoundException::new);
-        lastlyRetrieveProductRepository.save(new LastlyRetrieveProduct(member, product, Instant.now()));
-    }
+  private final MemberRepository memberRepository;
+  private final ProductRepository productRepository;
+  private final LastlyRetrieveProductRepository lastlyRetrieveProductRepository;
 
-    @Transactional
-    public List<ProductDTO> printLastlyRetrieveProduct(Long memberId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(EntityNotFoundException::new);
-        List<LastlyRetrieveProduct> lastlyRetrieveProducts =
-                lastlyRetrieveProductRepository.findLastlyRetrieveProductByMemberOrderByCreatedAtDesc(member);
+  @Transactional
+  public void saveLastlyRetrieveProduct(Long memberId, Long productId) {
+    Member member = memberRepository.findById(memberId).orElseThrow(EntityNotFoundException::new);
+    Product product = productRepository.findById(productId)
+        .orElseThrow(EntityNotFoundException::new);
+    lastlyRetrieveProductRepository.save(new LastlyRetrieveProduct(member, product, Instant.now()));
+  }
 
-        return lastlyRetrieveProducts.stream()
-                .map(LastlyRetrieveProduct::getProduct)
-                .map(it -> ProductMapper.toDto(it,  new Category()))
-                .toList();
-    }
+  @Transactional
+  public List<ProductDTO> printLastlyRetrieveProduct(Long memberId) {
+    Member member = memberRepository.findById(memberId).orElseThrow(EntityNotFoundException::new);
+    List<LastlyRetrieveProduct> lastlyRetrieveProducts =
+        lastlyRetrieveProductRepository.findTop7LastlyRetrieveProductByMemberOrderByCreatedAtDesc(
+            member);
+
+    return lastlyRetrieveProducts.stream()
+        .map(LastlyRetrieveProduct::getProduct)
+        .map(it -> ProductMapper.toDto(it, new Category()))
+        .toList();
+  }
 }
