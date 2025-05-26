757d9253e744243d326598dd973610cc04c8f769
twkim5235 <sssunggure@naver.com>
2025-05-26 20:50:14 +0900

    [FIX] 제품 검색 고도화 - 전체 상품 조회 시, paging 설정 - 높은, 낮은 가격순 정렬 출력 - 상품 이름 검색  - 조회 성능을 위한 title에 인덱스 설정

--- src/main/java/com/example/ddd_start/product/domain/Product.java
+++ src/main/java/com/example/ddd_start/product/domain/Product.java
@@ -13,12 +13,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "product", indexes = @Index(name = "idx_title", columnList = "title"))
 @Getter
 @NoArgsConstructor
 public class Product {
