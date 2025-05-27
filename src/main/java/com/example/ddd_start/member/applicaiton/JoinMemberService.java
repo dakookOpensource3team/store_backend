e49d56a003aa44a89ce9a6a62654ba22b3d6998b
twkim5235 <sssunggure@naver.com>
2025-05-27 19:33:57 +0900

    [HOTFIX] 회원가입 수정 - id 중복 확인

--- src/main/java/com/example/ddd_start/member/applicaiton/JoinMemberService.java
+++ src/main/java/com/example/ddd_start/member/applicaiton/JoinMemberService.java
@@ -2,6 +2,7 @@ package com.example.ddd_start.member.applicaiton;
 
 import com.example.ddd_start.common.domain.Address;
 import com.example.ddd_start.common.domain.exception.DuplicateEmailException;
+import com.example.ddd_start.common.domain.exception.DuplicateUsernameException;
 import com.example.ddd_start.member.applicaiton.model.AddressCommand;
 import com.example.ddd_start.member.applicaiton.model.joinCommand;
 import com.example.ddd_start.member.applicaiton.model.joinResponse;
@@ -16,51 +17,52 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class JoinMemberService {
 
-    private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+  private final MemberRepository memberRepository;
+  private final PasswordEncoder passwordEncoder;
 
-    @Transactional
-    public joinResponse joinMember(joinCommand req) throws DuplicateEmailException {
-        //값의 형식 검사
-        checkEmpty(req.getEmail(), "email");
-        checkEmpty(req.getPassword(), "password");
-        checkEmpty(req.getUsername(), "username");
-        checkEmpty(req.getRole(), "role");
+  @Transactional
+  public joinResponse joinMember(joinCommand req)
+      throws DuplicateEmailException, DuplicateUsernameException {
+    //값의 형식 검사
+    checkEmpty(req.getEmail(), "email");
+    checkEmpty(req.getPassword(), "password");
+    checkEmpty(req.getUsername(), "username");
+    checkEmpty(req.getRole(), "role");
 
-        //로직 검사
-        checkDuplicatedEmail(req.getEmail());
-        checkDuplicatedUsername(req.getUsername());
+    //로직 검사
+    checkDuplicatedEmail(req.getEmail());
+    checkDuplicatedUsername(req.getUsername());
 
-        AddressCommand addressReq = req.getAddressReq();
-        Address address = new Address(addressReq.getCity(), addressReq.getGuGun(), addressReq.getDong(),
-                addressReq.getBunji());
+    AddressCommand addressReq = req.getAddressReq();
+    Address address = new Address(addressReq.getCity(), addressReq.getGuGun(), addressReq.getDong(),
+        addressReq.getBunji());
 
-        String encryptedPassword = passwordEncoder.encode(req.getPassword());
-        Member member = new Member(req.getUsername(), req.getEmail(), encryptedPassword, address,
-            req.getRole());
+    String encryptedPassword = passwordEncoder.encode(req.getPassword());
+    Member member = new Member(req.getUsername(), req.getEmail(), encryptedPassword, address,
+        req.getRole());
 
-        memberRepository.save(member);
+    memberRepository.save(member);
 
-        return new joinResponse(member.getId(), member.getUsername());
+    return new joinResponse(member.getId(), member.getUsername());
+  }
+
+  private void checkDuplicatedEmail(String email) throws DuplicateEmailException {
+    Long count = memberRepository.countByEmail(email);
+    if (count > 0) {
+      throw new DuplicateEmailException();
     }
+  }
 
-    private void checkDuplicatedEmail(String email) throws DuplicateEmailException {
-        Long count = memberRepository.countByEmail(email);
-        if (count > 0) {
-            throw new DuplicateEmailException();
-        }
+  private void checkDuplicatedUsername(String userName) throws DuplicateUsernameException {
+    Long count = memberRepository.countByUsername(userName);
+    if (count > 0) {
+      throw new DuplicateUsernameException();
     }
+  }
 
-    private void checkDuplicatedUsername(String userName) throws DuplicateEmailException {
-        Long count = memberRepository.countByEmail(userName);
-        if (count > 0) {
-            throw new DuplicateEmailException();
-        }
+  private void checkEmpty(String value, String propertyName) {
+    if (value == null || value.isEmpty()) {
+      throw new NullPointerException(propertyName);
     }
-
-    private void checkEmpty(String value, String propertyName) {
-        if (value == null || value.isEmpty()) {
-            throw new NullPointerException(propertyName);
-        }
-    }
+  }
 }
