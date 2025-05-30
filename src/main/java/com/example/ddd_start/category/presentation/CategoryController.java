e73b411dff1fc620038cbcbfc7d5d4fd1d21faf9
twkim5235 <sssunggure@naver.com>
2025-05-30 09:05:46 +0900

    [FIX] 리팩토링 - 카테고리 조회 추가

---
+++ src/main/java/com/example/ddd_start/category/presentation/CategoryController.java
@@ -0,0 +1,19 @@
+package com.example.ddd_start.category.presentation;
+
+import com.example.ddd_start.category.application.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class CategoryController {
+
+  private final CategoryService categoryService;
+
+  @GetMapping("/categories")
+  public ResponseEntity printAllCategories() {
+    return ResponseEntity.ok(categoryService.findAll());
+  }
+}
